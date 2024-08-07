7b8bf81de5f3f021b0dd7cea14052d117df7bfef
Zawadi2 <zawadin95@gmail.comm>
2024-08-07 17:04:25 -0500

    created NowPlay.jsx

--- src/services/components/NowPlaying.jsx
+++ src/services/components/NowPlaying.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 const NowPlaying = ({ track }) => {
   return (
+    <>
     <div>
       <h2>Now Playing</h2>
       <p>{track.title} by {track.artist}</p>
     </div>
+    </>
   );
 };
 export default NowPlaying;
