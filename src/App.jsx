cc5981b62159c6cf6405484818c590fc57c9a546
tnleibel <tnleibel@outlook.com>
2024-08-08 14:03:02 -0400

    Ready to pull

--- src/App.jsx
+++ src/App.jsx
@@ -50,12 +50,8 @@ const App = () => {
       <NowPlaying track={currentTrack} />
       <Routes>
         <Route path="/tracks/add-track" element={<TrackForm handleAddTrack={handleAddTrack} />} />
-<<<<<<< HEAD
-        <Route path="/tracks" element={<TrackList tracks={tracks} setTracks={setTracks} handleDeleteTrack={handleDeleteTrack}/>} />    
-=======
         <Route path="/tracks" element={<TrackList tracks={tracks} setTracks={setTracks} />} />
         <Route path="/tracks/edit-track/:trackId" element={<TrackForm handleUpdateTrack={handleUpdateTrack} />} />
->>>>>>> origin
       </Routes>
     </>
   )
