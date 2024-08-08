e66df1569c4b27cf9532b16226affbaa5e43ee1c
Shalom <samsomshalom@yahoo.com>
2024-08-08 08:30:22 -0700

    added api delete and fetchall functionalities to trackservice and added a handledeletetrack function that handles deletion of a track by calling an api delete request to the backend and deleting from the db. Also added a route to display all tracks

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import * as trackService from './services/trackService'
 import { useState } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import TrackForm from './components/TrackForm';
-import NowPlaying from './NowPlaying';
+import NowPlaying from './components/NowPlaying';
+import TrackList from './components/TrackList';
+
 
 const App = () => {
   const [tracks, setTracks] = useState([])
@@ -14,11 +16,25 @@ const App = () => {
     navigate('./tracks')
   }
 
+  const handleDeleteTrack = async (trackId) => {
+    await trackService.del(trackId);
+    setTracks(tracks.filter(track => 
+      track._id !== trackId
+    ));
+  }
+
+  const handleUpdateTrack = () => {}
+  {/* need to add an edit route and a function to handle update of the track
+  -- also prolly will need to make an api update request to update the db??? */}
   return (
     <>
+      <TrackList handleDeleteTrack={handleDeleteTrack} tracks={tracks} setTracks={setTracks} />
+      
       <Routes>
         <Route path="/tracks/add-track" element={<TrackForm handleAddTrack={handleAddTrack} />} />
+        <Route path="/tracks" element={<TrackList tracks={tracks} setTracks={setTracks} />} />
       </Routes>
+      
     </>
   )
 };
